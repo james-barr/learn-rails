b74a4f95cd487a3d6a3e285796a3bd86dc5fae90
James <jamesbarr711@gmail.com>
2017-06-02 18:06:59 -0600

    add tests

---
+++ test/integration/home_page_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+# Feature: home page
+# As a visitor
+# I want to visit a home page
+# so i can learn more about the website
+
+feature 'Home page' do
+
+  # scenario: visit the home page
+  #   given i am a visitor
+  # when i visit the home page
+  # then i see 'welcome'
+  scenario 'visit the home page' do
+    visit root_path
+    page.must_have_content 'Stay in touch'
+  end
+end
